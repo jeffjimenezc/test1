45df39682b7fc8b7af9a3f62cb79144c8e2ba97c
jeffjimenezc@gmail.com <UNA@DESKTOP-5S1UEM8>
2023-09-28 12:17:53 -0600

    commit 1

--- test1/src/main/java/com/mycompany/test1/Test1.java
+++ test1/src/main/java/com/mycompany/test1/Test1.java
@@ -13,5 +13,7 @@ public class Test1 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        System.out.println("this is part of the second commit-push");
     }
 }
